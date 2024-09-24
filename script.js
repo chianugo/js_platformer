9487209516106ebe7ac24ae2099dc79d5f9fff80
cko003 <cko003@bucknell.edu>
2024-09-24 19:34:45 +0100

    create player object and update gravity

--- script.js
+++ script.js
@@ -4,19 +4,33 @@ const context = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-let y = 100;
-let x = 100;
-maxHeight = 576;
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw() {
+    context.fillStyle = "red";
+    context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+const player = new Player();
+
 function animate() {
   window.requestAnimationFrame(animate);
-  // Draw Player
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.fillStyle = "white";
   context.fillRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "red";
-  context.fillRect(x, y, 100, 100);
-  if (y <= maxHeight - 100) {
-    y++;
+  // Draw Player
+  player.draw();
+
+  if (player.position.y <= canvas.height - player.height) {
+    player.position.y += 2;
   }
 }
 
