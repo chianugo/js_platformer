18bcd4d92a85a609b45eaae2e1afdb4fcb800870
cko003 <cko003@bucknell.edu>
2024-09-24 19:12:20 +0100

    add gravity to player

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ const context = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-context.fillStyle = "white";
-context.fillRect(0, 0, canvas.width, canvas.height);
+let y = 100;
+let x = 100;
+function animate() {
+  window.requestAnimationFrame(animate);
+  // Draw Player
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "red";
+  context.fillRect(x, y, 100, 100);
+  y++;
+}
+
+animate();
