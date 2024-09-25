573419e9aa866febe3f8eb9e131fc331d622824a
cko003 <cko003@bucknell.edu>
2024-09-25 16:30:43 +0100

    move event listeners to its own file

---
+++ js/eventListeners.js
@@ -0,0 +1,36 @@
+window.addEventListener("keydown", (event) => {
+  // console.log(event.key);
+  switch (event.key) {
+    case "w":
+    case "ArrowUp":
+      console.log(event.key);
+      // if (player.velocity.y === 0)
+      player.velocity.y = -20;
+      break;
+    case "a":
+    case "ArrowLeft":
+      keys.a.isPressed = true;
+      break;
+    case "ArrowDown":
+      break;
+    case "d":
+    case "ArrowRight":
+      keys.d.isPressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "a":
+    case "ArrowLeft":
+      keys.a.isPressed = false;
+      break;
+    case "ArrowDown":
+      break;
+    case "d":
+    case "ArrowRight":
+      keys.d.isPressed = false;
+      break;
+  }
+});
