d6f02dfac78b404231ed0fbb0290bba5de81ccdc
cko003 <cko003@bucknell.edu>
2024-09-27 16:22:03 +0100

    remove sprite class fromm scripts and add to its own class file

--- js/script.js
+++ js/script.js
@@ -4,19 +4,6 @@ const context = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-// TODO Add image source
-class Sprite {
-  constructor({ position, imageSrc }) {
-    this.position = position;
-    this.image = new Image();
-    this.image.src = imageSrc;
-  }
-
-  draw() {
-    context.drawImage(this.image, this.position.x, this.position.y);
-  }
-}
-
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
